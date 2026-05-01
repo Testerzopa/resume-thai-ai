c4b8c9aa0952fc260b61a30e09510fe3886d3de4
Testerzopa <testerzopa111@gmail.com>
2026-05-01 19:39:00 +0700

    build live preview and photo preview

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,20 +1,83 @@
+import { useState } from "react"
 import "./components-style/Contents.css"
 function Content(){
+    const [photo , setPhoto] = useState(null)
+    const [fullName , setFullname] = useState("")
+    const [email , setEmail] = useState("")
+    const [phone , setPhone] = useState("")
+
+    const [location , setLocation] = useState("")
+
+    const [skills , setSkills] = useState("")
     return(
         <section className="content-layout">
             
             <div className="form-resume">
                 <form className="form-container">
                     <h2>Create Resume</h2>
-                    <input type="text" />
-                    <input type="email" name="" id="" />
-                    <input type="text" />
-                    <input type="text" />
-                    <input type="text" />
+                    
+                    <p>เลือกรูปโปรไฟล์ของคุณ</p>
+                    <input
+                    className="btn-photo"
+                    type="file"
+                    accept=".png,.jpg,.jpeg"
+                    onChange={(e)=>{
+                        const file = e.target.files[0]
+                        if(file){
+                            setPhoto(URL.createObjectURL(file))
+                        }
+                    }}
+                    />
+
+                    <input type="text"
+                    placeholder="Fullname"
+                    value={fullName}
+                    onChange={(e)=>setFullname(e.target.value)}
+                    />
+
+                    <input type="email"
+                    placeholder="Email"
+                    value={email}
+                    onChange={(e)=>setEmail(e.target.value)}
+                    />
+
+                    <input type="tel"
+                    placeholder="Phone Number"
+                    value={phone}
+                    onChange={(e)=>setPhone(e.target.value)}
+                    />
+
+                    <h4>Location</h4>
+                    <input type="text"
+                    placeholder="City"
+                    value={location}
+                    onChange={(e)=>setLocation(e.target.value)}
+                    />
+                    
+
+                    <h4>Skills</h4>
+                    <input type="text"
+                    placeholder="Skills"
+                    value={skills}
+                    onChange={(e)=>setSkills(e.target.value)}
+                    />
+                    
+
                 </form>
             </div>
             <div className="preview">
-                <div className="preview-container"></div>
+                <div className="preview-container">
+                    {photo && <img src={photo} alt="profile" />}
+                    <h1>{fullName || "Your name"}</h1>
+                    <p>{email || "email@gmail.com"}</p>
+                    <p>{phone || "Number"}</p>
+
+                    <h2>Location</h2>
+                    <p>{location || "location"}</p>
+
+                    <h2>Skills</h2>
+                    <p>{skills || "skill"}</p>
+                </div>
             </div>
         </section>
     )
